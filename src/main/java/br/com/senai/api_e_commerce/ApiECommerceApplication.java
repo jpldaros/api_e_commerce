a2c314d158c1243b783e2cbb18f68aa3e5793570
jpldaros <jpldaros@gmail.com>
2026-03-04 09:09:14 -0300

    commit inicial

---
+++ src/main/java/br/com/senai/api_e_commerce/ApiECommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/api_e_commerce/ApiECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
